0a2fb19c0321a4be768cb4dc93c90350cb7922da
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2024-03-15 20:09:09 -0400

    update issues script

--- Scripts/03-Ch3_issues_data.R
+++ Scripts/03-Ch3_issues_data.R
@@ -11,6 +11,7 @@
 library(tidyverse)
 
 #### Read in cleaned dataset ####
+# Outputs/Data/
 debate_questions_cleaned <- read_csv("Outputs/Data/debate_questions_cleaned.csv")
 
 #### Create specific datasets by year ####
@@ -99,7 +100,7 @@ debate_questions_2021 =
          debate_lang) 
 debate_questions_2021
 
-#### Issue stats ####
+#### Primary & Secondary Issue Stats ####
 # Code referenced from: https://stackoverflow.com/questions/76064350/count-number-of-rows-where-a-value-appears-in-any-of-two-columns-in-r
 ## 2008 ##
 # EN #
@@ -180,31 +181,70 @@ debate_questions_2011_all =
 debate_questions_2011_all 
 
 ## 2015 ##
-# EN #
-debate_questions_2015_EN =
+# TVA #
+debate_questions_2015_TVA =
   debate_questions_2015 |>
-  filter(language_of_question == "English") |>
+  filter(debate_number == "2015_TVA") |>
   select(primary_issue, secondary_issue) |>
   mutate(across(everything(), trimws)) |>
   rowid_to_column() |>
   pivot_longer(-rowid) |>
   group_by(value) |>
   summarise(n = n_distinct(rowid)) |>
-  mutate(proportion = n / (sum(26))) 
-debate_questions_2015_EN
+  mutate(proportion = n / (sum(33))) 
+debate_questions_2015_TVA
 
-# FR #
-debate_questions_2015_FR =
+# Radio-Canada #
+debate_questions_2015_RC =
   debate_questions_2015 |>
-  filter(language_of_question == "French") |>
+  filter(debate_number == "2015_Radio-Canada") |>
   select(primary_issue, secondary_issue) |>
   mutate(across(everything(), trimws)) |>
   rowid_to_column() |>
   pivot_longer(-rowid) |>
   group_by(value) |>
   summarise(n = n_distinct(rowid)) |>
-  mutate(proportion = n / (sum(50))) 
-debate_questions_2015_FR
+  mutate(proportion = n / (sum(17))) 
+debate_questions_2015_RC
+
+# Munk #
+debate_questions_2015_Munk =
+  debate_questions_2015 |>
+  filter(debate_number == "2015_Munk") |>
+  select(primary_issue, secondary_issue) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(9))) 
+debate_questions_2015_RC
+
+# Macleans #
+debate_questions_2015_Mac =
+  debate_questions_2015 |>
+  filter(debate_number == "2015_Macleans") |>
+  select(primary_issue, secondary_issue) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(7))) 
+debate_questions_2015_Mac
+
+# Globe & Mail #
+debate_questions_2015_GM =
+  debate_questions_2015 |>
+  filter(debate_number == "2015_Globe&Mail") |>
+  select(primary_issue, secondary_issue) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(10))) 
+debate_questions_2015_GM
 
 # All 2015 debates #
 debate_questions_2015_all =
@@ -219,31 +259,57 @@ debate_questions_2015_all =
 debate_questions_2015_all
 
 ## 2019 ##
-# EN #
+# LDC EN #
 debate_questions_2019_EN =
   debate_questions_2019 |>
-  filter(language_of_question == "English") |>
+  filter(debate_number == "2019_LDC_en") |>
   select(primary_issue, secondary_issue) |>
   mutate(across(everything(), trimws)) |>
   rowid_to_column() |>
   pivot_longer(-rowid) |>
   group_by(value) |>
   summarise(n = n_distinct(rowid)) |>
-  mutate(proportion = n / (sum(29))) 
+  mutate(proportion = n / (sum(18))) 
 debate_questions_2019_EN
 
-# FR #
+# LDC FR #
 debate_questions_2019_FR =
   debate_questions_2019 |>
-  filter(language_of_question == "French") |>
+  filter(debate_number == "2019_LDC_fr") |>
   select(primary_issue, secondary_issue) |>
   mutate(across(everything(), trimws)) |>
   rowid_to_column() |>
   pivot_longer(-rowid) |>
   group_by(value) |>
   summarise(n = n_distinct(rowid)) |>
-  mutate(proportion = n / (sum(79))) 
-debate_questions_2019_FR
+  mutate(proportion = n / (sum(40))) 
+debate_questions_2019_FR 
+
+# TVA #
+debate_questions_2019_TVA =
+  debate_questions_2019 |>
+  filter(debate_number == "2019_TVA") |>
+  select(primary_issue, secondary_issue) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(40))) 
+debate_questions_2019_TVA 
+
+# Macleans #
+debate_questions_2019_Mac =
+  debate_questions_2019 |>
+  filter(debate_number == "2019_Macleans") |>
+  select(primary_issue, secondary_issue) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(11))) 
+debate_questions_2019_Mac
 
 # All 2019 debates #
 debate_questions_2019_all =
@@ -258,10 +324,10 @@ debate_questions_2019_all =
 debate_questions_2019_all
 
 ## 2021 ##
-# EN #
+# LDC EN #
 debate_questions_2021_EN =
   debate_questions_2021 |>
-  filter(language_of_question == "English") |>
+  filter(debate_number == "2021_LDC_en") |>
   select(primary_issue, secondary_issue) |>
   mutate(across(everything(), trimws)) |>
   rowid_to_column() |>
@@ -271,18 +337,31 @@ debate_questions_2021_EN =
   mutate(proportion = n / (sum(45)))
 debate_questions_2021_EN 
 
-# FR #
+# LDC FR #
 debate_questions_2021_FR =
   debate_questions_2021 |>
-  filter(language_of_question == "French") |>
+  filter(debate_number == "2021_LDC_fr") |>
   select(primary_issue, secondary_issue) |>
   mutate(across(everything(), trimws)) |>
   rowid_to_column() |>
   pivot_longer(-rowid) |>
   group_by(value) |>
   summarise(n = n_distinct(rowid)) |>
-  mutate(proportion = n / (sum(90)))
-debate_questions_2021_FR
+  mutate(proportion = n / (sum(50)))
+debate_questions_2021_FR 
+
+# TVA #
+debate_questions_2021_TVA =
+  debate_questions_2021 |>
+  filter(debate_number == "2021_TVA") |>
+  select(primary_issue, secondary_issue) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(40)))
+debate_questions_2021_TVA 
 
 # All 2021 debates #
 debate_questions_2021_all =
@@ -294,4 +373,59 @@ debate_questions_2021_all =
   group_by(value) |>
   summarise(n = n_distinct(rowid)) |>
   mutate(proportion = n / (sum(135)))
-debate_questions_2021_all
+debate_questions_2021_all |> print(n =25)
+
+#### Territorial Issue Stats ####
+## 2008 ##
+# EN #
+debate_qs_2008_EN_territory =
+  debate_questions_2008 |>
+  filter(debate_number == "2008_Consortium_en") |>
+  select(territory1, territory2) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(8))) 
+debate_qs_2008_EN_territory
+
+# FR #
+debate_qs_2008_FR_territory =
+  debate_questions_2008 |>
+  filter(debate_number == "2008_Consortium_Fr") |>
+  select(territory1, territory2) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(14))) 
+debate_qs_2008_FR_territory
+
+## 2011 ##
+# EN #
+debate_qs_2011_EN_territory =
+  debate_questions_2011 |>
+  filter(debate_number == "2011_Consortium_en") |>
+  select(territory1, territory2) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(6))) 
+debate_qs_2011_EN_territory
+
+# FR #
+debate_qs_2011_FR_territory =
+  debate_questions_2011 |>
+  filter(debate_number == "2011_Consortium_Fr") |>
+  select(territory1, territory2) |>
+  mutate(across(everything(), trimws)) |>
+  rowid_to_column() |>
+  pivot_longer(-rowid) |>
+  group_by(value) |>
+  summarise(n = n_distinct(rowid)) |>
+  mutate(proportion = n / (sum(10))) 
+debate_qs_2011_FR_territory
