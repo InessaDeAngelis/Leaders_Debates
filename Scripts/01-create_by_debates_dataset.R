69f0ba848cb675c30974b26dd0db7b5098d782bf
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2024-07-09 19:58:30 -0400

    fix

--- Scripts/01-create_by_debates_dataset.R
+++ Scripts/01-create_by_debates_dataset.R
@@ -13,19 +13,21 @@ library(tidyverse)
 ## DQI by speech dataset ##
 dqi_by_speech_final <- read_csv(file = "Inputs/Data/dqi_by_speech_final.csv")
 
-## Debate questions dataset ####
+## Debate questions dataset ##
 debate_questions_cleaned <- read_csv(file = "Outputs/Data/debate_questions_cleaned.csv")
 
 ## Newspaper dataset ##
 newspaper_data_final <- read_csv(file = "Outputs/Data/newspaper_data_final.csv")
 
 #### Calculate "dqi_percent_demands" column for all debates ####
+## Count total number of interventions per debate ##
 dqi_by_speech_all =
   dqi_by_speech_final |>
   group_by(Debate_number) |>
   count() |>
   rename(total_number = n)
 
+## Count number of interventions coded as "having demands" per debate ##
 dqi_by_speech_all_demands =
   dqi_by_speech_final |>
   filter(Presence_of_demands == 1) |>
@@ -33,13 +35,15 @@ dqi_by_speech_all_demands =
   count() |>
   rename(number_of_demands = n)
 
+## Combine number of demands and total number of interventions datasets by debate number ##
 dqi_by_speech_all_combined <- merge(dqi_by_speech_all_demands, dqi_by_speech_all)
 
+## Divide number of demands by the total number of interventions by debate ##
 dqi_by_speech_all_final <- dqi_by_speech_all_combined |> mutate (number_of_demands / total_number) |>
   rename(
     dqi_percent_demands = "number_of_demands/total_number"
   ) |>
   select(Debate_number, dqi_percent_demands)
-dqi_by_speech_all_combined
+dqi_by_speech_all_final
 
 
