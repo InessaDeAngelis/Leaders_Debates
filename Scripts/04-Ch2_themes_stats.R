d86091314033d605053f5fc94f6eaa4ea2d57eb7
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2024-09-30 16:40:52 -0400

    finished themes re-code

--- Scripts/04-Ch2_themes_stats.R
+++ Scripts/04-Ch2_themes_stats.R
@@ -29,50 +29,49 @@ debate_questions_themes <-
     Theme == "The Pandemic" ~ "Covid", 
     Theme == "Pandemic & Health" ~ "Covid", 
     Theme == "COVID RECOVERY" ~ "Covid", 
-    Theme == "Canada in the Future" ~ "Canada in the future",
-    Theme == "energy and the environment." ~ "Environment and energy",
-    Theme == "The environment and energy" ~ "Environment and energy",
-    Theme == "Energy and Environment" ~ "Environment and energy",
-    Theme == "Economy & Environment" ~ "Economy and environment",
-    Theme == "Canada in the Future" ~ "Canada in the future",
+    Theme == "Canada in the Future" ~ "Other",
+    Theme == "energy and the environment." ~ "Environment",
+    Theme == "The environment and energy" ~ "Environment",
+    Theme == "Energy and Environment" ~ "Energy",
+    Theme == "Economy & Environment" ~ "Economy",
     Theme == "Canada in the world" ~ "Foreign affairs",
     Theme == "leadership in Canada and the world" ~ "Foreign affairs",
     Theme == "Canada on the world stage" ~ "Foreign affairs",
     Theme == "foreign policy and security" ~ "Foreign affairs",
-    Theme == "Canada's Democracy" ~ "Governance and democracy",
+    Theme == "Canada's Democracy" ~ "Ethics",
     Theme == "Jobs" ~ "Jobs", 
     Theme == "housing" ~ "Housing",
     Theme == "infrastructure" ~ "Infrastructure",
-    Theme == "taxation" ~ "Taxation",
+    Theme == "taxation" ~ "Economy",
     Theme == "immigration" ~ "Immigration",
     Theme == "Immigration, Social Politics" ~ "Immigration",
-    Theme == "Governance, democracy, and institutions" ~ "Governance and democracy",
-    Theme == "Leadership and governance" ~ "Governance and democracy",
-    Theme == "Governance and democracy" ~ "Governance and democracy",
-    Theme == "Government services" ~ "Government and citizen services",
-    Theme == "Canadian Security" ~ "Canadian security",
+    Theme == "Governance, democracy, and institutions" ~ "Constitution",
+    Theme == "Leadership and governance" ~ "Government formation",
+    Theme == "Governance and democracy" ~ "Government formation",
+    Theme == "Government services" ~ "Other",
+    Theme == "Canadian Security" ~ "Defence",
     Theme == "Social Policy & Governance" ~ "Social welfare",
-    Theme == "Environment" ~ "Environment and energy",
-    Theme == "Climate Change" ~ "Environment and energy",
+    Theme == "Environment" ~ "Environment",
+    Theme == "Climate Change" ~ "Environment",
     Theme == "polarization, human rights, and immigration" ~ "Immigration",
-    Theme == "affordability and income security"  ~ "Affordability",
+    Theme == "affordability and income security"  ~ "Economy",
     Theme == "Foreign Affairs & Immigration" ~ "Foreign affairs",
-    Theme == "Citizen Services" ~ "Government and citizen services",
-    Theme == "Identity, Ethics, & Governence" ~ "Governance and democracy",
-    Theme == "Justice & Foreign Affairs" ~ "Justice and foreign affairs",
+    Theme == "Citizen Services" ~ "Other",
+    Theme == "Identity, Ethics, & Governence" ~ "Ethics",
+    Theme == "Justice & Foreign Affairs" ~ "Justice",
     Theme == "Indigenous Identity & Culture" ~ "Indigenous",
     Theme == "Indigenous Issues" ~ "Indigenous",
     Theme == "Reconciliation" ~ "Indigenous",
     Theme == "Crime" ~ "Crime",
-    Theme == "Culture and national identity" ~ "Culture and national identity",
+    Theme == "Culture and national identity" ~ "Culture and heritage",
     Theme == "Health care" ~ "Health care",
-    Theme == "Quebec's place in Canada" ~ "Governance and Quebec",
-    Theme == "Values" ~ "Values",
+    Theme == "Quebec's place in Canada" ~ "Constitution",
+    Theme == "Values" ~ "Other",
     Theme == "Social policy" ~ "Social welfare",
     Theme == "Social Policy" ~ "Social welfare",
-    Theme == "Governance & Quebec" ~"Governance and Quebec",
-    Theme == "Affordability" ~ "Affordability",
-    Theme == "LEADERSHIP AND ACCOUNTABILITY" ~ "Governance and democracy")) 
+    Theme == "Governance & Quebec" ~"Constitution",
+    Theme == "Affordability" ~ "Economy",
+    Theme == "LEADERSHIP AND ACCOUNTABILITY" ~ "Ethics")) 
 
 ## Save dataset ##
 write_csv(debate_questions_themes, "Outputs/Data/debate_questions_themes.csv")
@@ -87,13 +86,14 @@ debate_questions_themes |>
   ungroup() |>
   rename(Number = n) |>
   mutate (Percent = percent(Number / sum(Number), digit = 1)) |>
-  print(n = 25) 
+  print(n = 20) 
 
 new_df <- themes_summary_stats |> 
   arrange(desc(Percent)) |>
-  print(n = 25)
+  print(n = 20)
 
-#### Re-code categories (only clean up existing categories) ####
+#### Old code ####
+## Re-code categories (only clean up existing categories) ##
 debate_questions_themes2 <-
   debate_questions_final |>
   select(Year, Debate_number, Theme) |>
