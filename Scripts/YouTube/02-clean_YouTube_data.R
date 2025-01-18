1474c288df25dfa05e38fb57e1438613c38c1998
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2025-01-18 10:56:15 -0500

    croissant break time

--- Scripts/YouTube/02-clean_YouTube_data.R
+++ Scripts/YouTube/02-clean_YouTube_data.R
@@ -10,94 +10,48 @@
 library(tidyverse)
 library(cld2)
 
-#### Read in data ####
-list_of_files <- list.files(path = "Inputs/Data/YouTube/",
-                            recursive = TRUE,
-                            pattern = "\\.csv$",
-                            full.names = TRUE)
+#### Read in datasets ####
+debate_2015_commments_GM <- read_csv("Inputs/Data/YouTube/debate_2015_comments_GM.csv")
+debate_2015_commments_Mac <- read_csv("Inputs/Data/YouTube/debate_2015_comments_Mac.csv")
+EN_debate_2019_comments_CBC <- read_csv("Inputs/Data/YouTube/EN_debate_2019_comments_CBC.csv")
+EN_debate_2019_comments_CTV <- read_csv("Inputs/Data/YouTube/EN_debate_2019_comments_CTV.csv")
+FR_debate_2019_comments_CBC <- read_csv("Inputs/Data/YouTube/FR_debate_2019_comments_CBC.csv")
+FR_debate_2019_comments_GN <- read_csv("Inputs/Data/YouTube/FR_debate_2019_comments_GN.csv")
+EN_debate_2021_comments_CTV <- read_csv("Inputs/Data/YouTube/EN_debate_2021_comments_CTV.csv")
+EN_debate_2021_comments_GN <- read_csv("Inputs/Data/YouTube/EN_debate_2021_comments_GN.csv")
+FR_debate_2021_comments_GN <- read_csv("Inputs/Data/YouTube/FR_debate_2021_comments_GN.csv")
+FR_debate_2021_comments_GN2 <- read_csv("Inputs/Data/YouTube/FR_debate_2021_comments_GN2.csv")
 
-debate_comments <- readr::read_csv(list_of_files, id = "Debate_number")
+#### Clean datasets ####
+## Create function to filter comments between the day of the debate and the end of the writ period & add debate number ##
+process_comments <- function(df, start_date, end_date, Debate_number) {df |>
+    mutate(PublishedAt = as.Date(as.POSIXct(PublishedAt)),
+      Debate_number = Debate_number, .before = Comment) |>
+    filter(between(PublishedAt, as.Date(start_date), as.Date(end_date)))}
 
-## Remove file path info debate number column ##
-debate_comments$Debate_number <- str_remove(debate_comments$Debate_number, "Inputs/Data/YouTube//")
-debate_comments$Debate_number <- str_remove(debate_comments$Debate_number, ".csv")
+## Run function ##
+debate_2015_commments_GM <- process_comments(debate_2015_commments_GM, '2015-09-18', '2015-10-19', '2015Globe&Mail')
+debate_2015_commments_Mac <- process_comments(debate_2015_commments_Mac, '2015-08-07', '2015-10-19', '2015Macleans')
+EN_debate_2019_comments_CBC <- process_comments(EN_debate_2019_comments_CBC, '2019-10-08', '2019-10-21', '2019EnLDC')
+EN_debate_2019_comments_CTV <- process_comments(EN_debate_2019_comments_CTV, '2019-10-08', '2019-10-21', '2019EnLDC')
+FR_debate_2019_comments_CBC <- process_comments(FR_debate_2019_comments_CBC, '2019-10-11', '2019-10-21', '2019FrLDC')
+FR_debate_2019_comments_GN <- process_comments(FR_debate_2019_comments_GN, '2019-10-11', '2019-10-21', '2019FrLDC')
+EN_debate_2021_comments_CTV <- process_comments(EN_debate_2021_comments_CTV, '2021-09-10', '2021-09-20', '2021EnLDC')
+EN_debate_2021_comments_GN <- process_comments(EN_debate_2021_comments_GN, '2021-09-10', '2021-09-20', '2021EnLDC')
+FR_debate_2021_comments_GN <- process_comments(FR_debate_2021_comments_GN, '2021-09-09', '2021-09-20', '2021FrLDC')
+FR_debate_2021_comments_GN2 <- process_comments(FR_debate_2021_comments_GN2, '2021-09-09', '2021-09-20', '2021FrLDC')
 
-## Detect comment language ##
-# Rename column #
-debate_comments <- debate_comments |>
-  rename(text = Comment)
-
-# Detect languages #
-language_results <- detect_language(debate_comments$text)
-
-# Put language categorizations into df #
-df2 <- data.frame(language_results)
-
-# Combine language categorizations with the rest of the data #
-debate_comments2 <- cbind(debate_comments, df2)
-
-## ENGLISH: Drop comments in other languages, fix debate names, drop extra columns ##
-debate_comments_en =
-  debate_comments2 |>
-  filter(grepl('en',language_results)) |>
-  mutate("Outlet" = case_when(
-    Debate_number == "debate_2015_commments_GM" ~ "Globe & Mail",
-    Debate_number == "debate_2015_commments_Mac" ~ "Maclean's",
-    Debate_number == "EN_debate_2019_commments_CTV" ~ "CTV",
-    Debate_number == "EN_debate_2019_commments_CBC" ~ "CBC",
-    Debate_number == "FR_debate_2019_commments_GN" ~ "Global News",
-    Debate_number == "FR_debate_2019_commments_CBC" ~ "CBC",
-    Debate_number == "FR_debate_2021_commments_GN" ~ "Global News",
-    Debate_number == "EN_debate_2021_commments_CTV" ~ "CTV",
-    Debate_number == "EN_debate_2021_commments" ~ "Global News",
-    Debate_number == "FR_debate_2021_commments" ~ "Global News"), .before = text) |>
-  mutate("Debate_number" = case_when(
-    Debate_number == "debate_2015_commments_GM" ~ "2015Globe&Mail",
-    Debate_number == "debate_2015_commments_Mac" ~ "2015Macleans",
-    Debate_number == "EN_debate_2019_commments_CTV" ~ "2019EnLDC",
-    Debate_number == "EN_debate_2019_commments_CBC" ~ "2019EnLDC",
-    Debate_number == "FR_debate_2019_commments_GN" ~ "2019FrLDC",
-    Debate_number == "FR_debate_2019_commments_CBC" ~ "2019FrLDC",
-    Debate_number == "FR_debate_2021_commments_GN" ~ "2021FrLDC",
-    Debate_number == "FR_debate_2021_commments" ~ "2021FrLDC",
-    Debate_number == "EN_debate_2021_commments_CTV" ~ "2021EnLDC",
-    Debate_number == "EN_debate_2021_commments" ~ "2021EnLDC")) |>
-  rename(Comment = text) |>
-  select(-c(AuthorProfileImageUrl, AuthorChannelUrl, UpdatedAt, ReplyCount, LikeCount))
-debate_comments_en
-
-## Save dataset ##
-write_csv(debate_comments_en, "Outputs/Data/YouTube/debate_comments.csv")
-
-## French: Drop comments in other languages, fix debate names, drop extra columns ##
-debate_comments_fr =
-  debate_comments2 |>
-  filter(grepl('fr',language_results)) |>
-  mutate("Outlet" = case_when(
-    Debate_number == "debate_2015_commments_GM" ~ "Globe & Mail",
-    Debate_number == "debate_2015_commments_Mac" ~ "Maclean's",
-    Debate_number == "EN_debate_2019_commments_CTV" ~ "CTV",
-    Debate_number == "EN_debate_2019_commments_CBC" ~ "CBC",
-    Debate_number == "FR_debate_2019_commments_GN" ~ "Global News",
-    Debate_number == "FR_debate_2019_commments_CBC" ~ "CBC",
-    Debate_number == "FR_debate_2021_commments_GN" ~ "Global News",
-    Debate_number == "EN_debate_2021_commments_CTV" ~ "CTV",
-    Debate_number == "EN_debate_2021_commments" ~ "Global News",
-    Debate_number == "FR_debate_2021_commments" ~ "Global News"), .before = text) |>
-  mutate("Debate_number" = case_when(
-    Debate_number == "debate_2015_commments_GM" ~ "2015Globe&Mail",
-    Debate_number == "debate_2015_commments_Mac" ~ "2015Macleans",
-    Debate_number == "EN_debate_2019_commments_CTV" ~ "2019EnLDC",
-    Debate_number == "EN_debate_2019_commments_CBC" ~ "2019EnLDC",
-    Debate_number == "FR_debate_2019_commments_GN" ~ "2019FrLDC",
-    Debate_number == "FR_debate_2019_commments_CBC" ~ "2019FrLDC",
-    Debate_number == "FR_debate_2021_commments_GN" ~ "2021FrLDC",
-    Debate_number == "FR_debate_2021_commments" ~ "2021FrLDC",
-    Debate_number == "EN_debate_2021_commments_CTV" ~ "2021EnLDC",
-    Debate_number == "EN_debate_2021_commments" ~ "2021EnLDC")) |>
-  rename(Comment = text) |>
-  select(-c(AuthorProfileImageUrl, AuthorChannelUrl, UpdatedAt, ReplyCount, LikeCount))
-debate_comments_fr
-
-## Save dataset ##
-write_csv(debate_comments_fr, "Outputs/Data/YouTube/debate_comments_fr.csv")
+## Combine cleaned datasets into one ##
+debate_comments_raw <-
+  rbind(
+    debate_2015_commments_GM,
+    debate_2015_commments_Mac,
+    EN_debate_2019_comments_CBC,
+    EN_debate_2019_comments_CTV,
+    FR_debate_2019_comments_CBC,
+    FR_debate_2019_comments_GN,
+    EN_debate_2021_comments_CTV,
+    EN_debate_2021_comments_GN,
+    FR_debate_2021_comments_GN,
+    FR_debate_2021_comments_GN2)
+debate_comments_raw 
