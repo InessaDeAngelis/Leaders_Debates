2b121eb24f40f04a2cd20f013724ced0cfed7480
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2025-02-22 10:29:56 -0500

    making models

--- Scripts/04-Ch2_themes_stats.R
+++ Scripts/04-Ch2_themes_stats.R
@@ -16,7 +16,7 @@ debate_questions_final <- read_csv(file = "Outputs/Data/debate_questions_final_r
 #### Re-code categories (to align with Bastien & CES) ####
 debate_questions_themes <-
   debate_questions_final |>
-  select(Year, Debate_number, Theme) |>
+  select(ID, Year, Debate_number, Theme) |>
   mutate("Theme" = case_when(
     Theme == "None" ~ "None",
     Theme == "none" ~ "None",
@@ -76,99 +76,69 @@ debate_questions_themes <-
 ## Save dataset ##
 write_csv(debate_questions_themes, "Outputs/Data/debate_questions_themes.csv")
 
-## Generate summary stats ##
-themes_summary_stats =
-debate_questions_themes |>
-  drop_na(Theme) |>
-  filter(!Theme == "None") |>
+## Generate summary stats - old ##
+themes_summary_stats <- debate_questions_themes |>
   group_by(Theme) |>
-  count() |>
-  ungroup() |>
-  rename(Number = n) |>
-  mutate (Percent = percent(Number / sum(Number), digit = 1)) |>
-  print(n = 20) 
-
-new_df <- themes_summary_stats |> 
-  arrange(desc(Percent)) |>
-  print(n = 20)
-
-#### Old code ####
-## Re-code categories (only clean up existing categories) ##
-debate_questions_themes2 <-
-  debate_questions_final |>
-  select(Year, Debate_number, Theme) |>
-  mutate("Theme" = case_when(
-    Theme == "None" ~ "None",
-    Theme == "none" ~ "None",
-    Theme == "Economy" ~ "Economy",
-    Theme == "The economy" ~ "Economy",
-    Theme == "Economy & Public Finances" ~ "Economy and public finances",
-    Theme == "Economy and spending" ~ "Economy and spending",
-    Theme == "Cost of Living & Public Finances" ~ "Cost of living and public finances",
-    Theme == "Consumers/Cost of Living" ~ "Consumers and cost of living",
-    Theme == "The Pandemic" ~ "The Pandemic", 
-    Theme == "Pandemic & Health" ~ "Pandemic and health", 
-    Theme == "COVID RECOVERY" ~ "Covid recovery", 
-    Theme == "Canada in the Future" ~ "Canada in the future",
-    Theme == "energy and the environment." ~ "Energy and the environment",
-    Theme == "The environment and energy" ~ "The environment and energy",
-    Theme == "Energy and Environment" ~ "Energy and environment",
-    Theme == "Economy & Environment" ~ "Economy and environment",
-    Theme == "Canada in the Future" ~ "Canada in the future",
-    Theme == "Canada in the world" ~ "Canada in the world",
-    Theme == "leadership in Canada and the world" ~ "Leadership in Canada and the world",
-    Theme == "Canada on the world stage" ~ "Canada on the world stage",
-    Theme == "foreign policy and security" ~ "Foreign policy and security",
-    Theme == "Canada's Democracy" ~ "Canada's democracy",
-    Theme == "Jobs" ~ "Jobs", 
-    Theme == "housing" ~ "Housing",
-    Theme == "infrastructure" ~ "Infrastructure",
-    Theme == "taxation" ~ "Taxation",
-    Theme == "immigration" ~ "Immigration",
-    Theme == "Immigration, Social Politics" ~ "Immigration and social politics",
-    Theme == "Governance, democracy, and institutions" ~ "Governance, democracy and institutions",
-    Theme == "Leadership and governance" ~ "Leadership and governance",
-    Theme == "Governance and democracy" ~ "Governance and democracy",
-    Theme == "Government services" ~ "Government services",
-    Theme == "Canadian Security" ~ "Canadian security",
-    Theme == "Social Policy & Governance" ~ "Social policy and governance",
-    Theme == "Environment" ~ "Environment",
-    Theme == "Climate Change" ~ "Climate change",
-    Theme == "polarization, human rights, and immigration" ~ "Polarization, human rights and immigration",
-    Theme == "affordability and income security"  ~ "Affordability and income security",
-    Theme == "Foreign Affairs & Immigration" ~ "Foreign affairs and immigration",
-    Theme == "Citizen Services" ~ "Citizen services",
-    Theme == "Identity, Ethics, & Governence" ~ "Identity, ethics and governance",
-    Theme == "Justice & Foreign Affairs" ~ "Justice and foreign affairs",
-    Theme == "Indigenous Identity & Culture" ~ "Indigenous identity and culture",
-    Theme == "Indigenous Issues" ~ "Indigenous issues",
-    Theme == "Reconciliation" ~ "Reconciliation",
-    Theme == "Crime" ~ "Crime",
-    Theme == "Culture and national identity" ~ "Culture and national identity",
-    Theme == "Health care" ~ "Health care",
-    Theme == "Quebec's place in Canada" ~ "Quebec's place in Canada",
-    Theme == "Values" ~ "Values",
-    Theme == "Social policy" ~ "Social policy",
-    Theme == "Social Policy" ~ "Social policy",
-    Theme == "Governance & Quebec" ~"Governance and Quebec",
-    Theme == "Affordability" ~ "Affordability",
-    Theme == "LEADERSHIP AND ACCOUNTABILITY" ~ "Leadership and accountability")) 
-
-## Save dataset ##
-write_csv(debate_questions_themes2, "Outputs/Data/debate_questions_themes2.csv")
-
-## Generate summary stats ##
-themes_summary_stats2 =
-  debate_questions_themes2 |>
-  drop_na(Theme) |>
-  filter(!Theme == "None") |>
-  group_by(Debate_number, Theme) |>
   count() |>
   ungroup() |>
   rename(Number = n) |>
   mutate (Percent = percent(Number / sum(Number), digit = 1)) |>
   print(n = 65) 
 
-new_df2 <- themes_summary_stats2 |> 
+new_df <- themes_summary_stats |> 
   arrange(desc(Percent)) |>
   print(n = 65)
+
+themes_again <- debate_questions_themes |>
+  filter(!Theme == "None") |>
+  group_by(Debate_number, Theme) |>
+  summarise(covered = 1) |> # Mark it as '1' if covered
+  ungroup()
+
+## Crosstabs - new ##
+crosstabs_df <- table(debate_questions_themes$Debate_number, debate_questions_themes$Theme)
+print(crosstabs_df)
+
+df <- data.frame(crosstabs_df)
+
+df <- df |>
+  rename(Debate_number = Var1,
+         Theme = Var2) |>
+  arrange(Debate_number)
+
+write_csv(df, "crosstabs_df2.csv")
+
+df2 <- df |>
+  filter(!grepl('0', Freq))
+
+write_csv(df2, "themes_crosstabs.csv")
+
+## Binary version ##
+my_data_binary <- debate_questions_themes |>
+  group_by(Debate_number, Theme) |>
+  summarise(covered = 1) |> # Mark it as '1' if covered
+  ungroup()
+
+crosstab_df <- xtabs(~ Debate_number + Theme, data = my_data_binary)
+print(crosstab_df)
+
+df3 <- data.frame(crosstab_df) 
+
+df3 <- df3 |>
+  filter(Freq == 1) |>
+  filter(!Theme == "None") |>
+  arrange(Debate_number) |>
+  select(Debate_number, Theme)
+
+df4 <- df3 |> 
+  group_by(Theme) |>
+count() |>
+  ungroup() |>
+  rename(Number = n) |>
+  mutate (Percent = percent(Number / sum(Number), digit = 1)) |>
+  arrange(Number) |>
+  print(n = 20) 
+
+write_csv(df3, "df3.csv")
+
+write_csv(df4, "df4.csv")
--- Scripts/04-Ch4_analysis.R
+++ Scripts/04-Ch4_analysis.R
@@ -8,32 +8,85 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(summarytools)
 
 dqi_by_speech_final <- read_csv("Outputs/Data/dqi_by_speech_final.csv")
 
-#### Interruptions ####
-interruptions_dqi <- dqi_by_speech_final |>
-  filter(Interruption == "Interruption") |>
+## Add in demographic data ##
+dqi_by_speech_dem <- dqi_by_speech_final |>
+  filter(Speaker == "Harper" | Speaker == "Dion" | Speaker == "May"| Speaker == "Layton" | Speaker == "Duceppe" |
+           Speaker == "Ignatieff" | Speaker == "Trudeau" | Speaker == "Singh" | Speaker == "Singh" | Speaker == "Paul" |
+           Speaker == "Mulcair" | Speaker == "Blanchette" | Speaker == "Scheer" | Speaker == "O'Toole") |>
   mutate(Gender = if_else(Speaker %in% c("May", "Paul"), "Woman", "Man"), .after = Speaker) |>
   mutate(Background = if_else(Speaker %in% c("Singh", "Paul"), "Racialized", "White"), .after = Gender) |>
   mutate(Incumbent = if_else((Speaker == "Trudeau" & Election_year >= 2019) | 
-      (Speaker == "Harper" & Election_year <= 2015), 
-        "Incumbent", 
-        "Not Incumbent"), .after = Background)
+                               (Speaker == "Harper" & Election_year <= 2015), 
+                             "Incumbent", 
+                             "Not Incumbent"), .after = Background)
 
-write_csv(interruptions_dqi, "Outputs/Data/interruptions_dqi.csv")
+#### Summary stats ####
+## All speeches and interrupted speeches given by party leaders ##
+total_speeches <- dqi_by_speech_dem |> 
+  summarise(
+    interrupted_speeches = sum(Interruption == "Interruption", na.rm = TRUE),
+    total_speeches = n(),
+    percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
+total_speeches
 
-interruptions_dqi |> group_by(Gender, Incumbent) |> count() |> print(n=29)
+## All speeches and interrupted speeches by incumbency ##
+speeches_incumbency <- dqi_by_speech_dem |>
+  group_by(Incumbent) |>
+  summarise(
+    interrupted_speeches = sum(Interruption == "Interruption", na.rm = TRUE),
+    total_speeches = n(),
+    percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
+speeches_incumbency
 
-#### Respect for demands ####
-respect_dqi <- dqi_by_speech_final |>
-  filter(Respect_for_demands == "Respect") |>
-  mutate(Gender = if_else(Speaker %in% c("May", "Paul"), "Woman", "Man"), .after = Speaker) |>
-  mutate(Background = if_else(Speaker %in% c("Singh", "Paul"), "Racialized", "White"), .after = Gender) |>
-  mutate(Incumbent = if_else((Speaker == "Trudeau" & Election_year >= 2019) | 
-    (Speaker == "Harper" & Election_year <= 2015), 
-      "Incumbent", 
-      "Not Incumbent"), .after = Background)
+## All speeches and interrupted speeches by gender ##
+speeches_gender <- dqi_by_speech_dem |>
+  group_by(Gender) |>
+  summarise(
+    interrupted_speeches = sum(Interruption == "Interruption", na.rm = TRUE),
+    total_speeches = n(),
+    percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
+speeches_gender
 
-write_csv(respect_dqi, "Outputs/Data/respect_dqi.csv")
+## All speeches and interrupted speeches by background ##
+speeches_bg <- dqi_by_speech_dem |>
+  group_by(Background) |>
+  summarise(
+    interrupted_speeches = sum(Interruption == "Interruption", na.rm = TRUE),
+    total_speeches = n(),
+    percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
+speeches_bg
+
+## All speeches and interrupted speeches by gender + background ##
+speeches_gender_bg <- dqi_by_speech_dem |>
+  group_by(Gender, Background) |>
+  summarise(
+    interrupted_speeches = sum(Interruption == "Interruption", na.rm = TRUE),
+    total_speeches = n(),
+    percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
+speeches_gender_bg
+
+#### Make models ####
+## Analysis data ##
+dqi_in <-dqi_by_speech_dem |>
+  drop_na(Interruption)
+
+## Re-code variables ##
+dqi_in$Gender <- ifelse(dqi_in$Gender == "Man", 1, 0)
+dqi_in$Background <- ifelse(dqi_in$Background == "White", 1, 0)
+dqi_in$Incumbent <- ifelse(dqi_in$Incumbent == "Incumbent", 1, 0)
+dqi_in$Interruption <- ifelse(dqi_in$Interruption == "Interruption", 1, 0)
+
+## Model interruptions with gender and background as predictors (with interaction) ##
+model <- lm(Interruption ~ Gender * Background, data = dqi_in)
+summary(model)
+
+## Model interruptions with gender and background as predictors (without interaction) ##
+model2 <- lm(Interruption ~ Gender + Background, data = dqi_in)
+summary(model2)
+
+## Model interruptions by incumbency ##
+model3 <- lm(Interruption ~ Incumbent, data = dqi_in)
+summary(model3)
