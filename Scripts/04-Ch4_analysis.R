7ee7d839c871dbb19c30db69793a117cb1ed92db
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2025-02-23 09:38:19 -0500

    respect for demands

--- Scripts/04-Ch4_analysis.R
+++ Scripts/04-Ch4_analysis.R
@@ -38,7 +38,7 @@ dqi_by_speech_dem <- dqi_by_speech_final |>
     Speaker == "O'Toole" ~ "Conservative",
     Speaker == "Bernier" ~ "People's Party"), .after = Incumbent)
 
-#### Summary stats ####
+#### Summary stats - Interruptions ####
 ## All speeches and interrupted speeches given by party leaders ##
 total_speeches <- dqi_by_speech_dem |> 
   summarise(
@@ -92,7 +92,7 @@ speeches_party <- dqi_by_speech_dem |>
     percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
 speeches_party
 
-#### Make models ####
+#### Make models - Interruptions ####
 ## Analysis data ##
 dqi_in <-dqi_by_speech_dem |>
   drop_na(Interruption) |>
@@ -138,3 +138,129 @@ summary(model6)
 ## Model interruptions by gender, background, incumbency AND politicial affiliation ##
 model7 <- lm(Interruption ~ Gender + Background + Incumbent + Political_affiliation, data = dqi_in)
 summary(model7)
+
+#### Summary stats - Respect for demands ####
+## All speeches and respect for demands ##
+total_speeches_respect <- dqi_by_speech_dem |> 
+  summarise(
+    respected_speeches = sum(Respect_for_demands == "Respect", na.rm = TRUE),
+    total_speeches = n(),
+    percent_respect = round((sum(Respect_for_demands == "Respect", na.rm = TRUE) / n()) * 100, 2))
+total_speeches_respect
+
+## All speeches and respect by incumbency ##
+speeches_incumbency_respect <- dqi_by_speech_dem |>
+  group_by(Incumbent) |>
+  summarise(
+    respected_speeches = sum(Respect_for_demands == "Respect", na.rm = TRUE),
+    total_speeches = n(),
+    percent_respect = round((sum(Respect_for_demands == "Respect", na.rm = TRUE) / n()) * 100, 2))
+speeches_incumbency_respect 
+
+## All speeches and respect by gender ##
+speeches_gender_respect <- dqi_by_speech_dem |>
+  group_by(Gender) |>
+  summarise(
+    respected_speeches = sum(Respect_for_demands == "Respect", na.rm = TRUE),
+    total_speeches = n(),
+    percent_respect = round((sum(Respect_for_demands == "Respect", na.rm = TRUE) / n()) * 100, 2))
+speeches_gender_respect
+
+## All speeches and respect by background ##
+speeches_bg_respect <- dqi_by_speech_dem |>
+  group_by(Background) |>
+  summarise(
+    respected_speeches = sum(Respect_for_demands == "Respect", na.rm = TRUE),
+    total_speeches = n(),
+    percent_respect = round((sum(Respect_for_demands == "Respect", na.rm = TRUE) / n()) * 100, 2))
+speeches_bg_respect
+
+## All speeches and respect by gender + background ##
+speeches_genderbg_respect <- dqi_by_speech_dem |>
+  group_by(Gender, Background) |>
+  summarise(
+    respected_speeches = sum(Respect_for_demands == "Respect", na.rm = TRUE),
+    total_speeches = n(),
+    percent_respect = round((sum(Respect_for_demands == "Respect", na.rm = TRUE) / n()) * 100, 2))
+speeches_genderbg_respect
+
+## All speeches and respect by political party affiliation ##
+speeches_party_respect <- dqi_by_speech_dem |>
+  group_by(Political_affiliation) |>
+  summarise(
+    respected_speeches = sum(Respect_for_demands == "Respect", na.rm = TRUE),
+    total_speeches = n(),
+    percent_respect = round((sum(Respect_for_demands == "Respect", na.rm = TRUE) / n()) * 100, 2))
+speeches_party_respect
+
+#### Make models - Respect for demands ####
+## Analysis data ##
+dqi_respect <-dqi_by_speech_dem |>
+  drop_na(Respect_for_demands) |>
+  mutate(Political_affiliation = case_when(
+    Political_affiliation == "Conservative" ~ 1,
+    Political_affiliation == "Liberal" ~ 2,
+    Political_affiliation == "NDP" ~ 3,
+    Political_affiliation == "Green" ~ 4,
+    Political_affiliation == "Bloc Québécois" ~ 5,
+    Political_affiliation == "People's Party" ~ 6))
+
+## Re-code variables ##
+dqi_respect$Gender <- ifelse(dqi_respect$Gender == "Man", 1, 0)
+dqi_respect$Background <- ifelse(dqi_respect$Background == "White", 1, 0)
+dqi_respect$Incumbent <- ifelse(dqi_respect$Incumbent == "Incumbent", 1, 0)
+dqi_respect$Respect_for_demands <- ifelse(dqi_respect$Respect_for_demands == "Respect", 1, 0)
+dqi_respect$Political_affiliation <- factor(dqi_respect$Political_affiliation)
+
+## Model respect with gender and background as predictors (with interaction) ##
+mod <- lm(Respect_for_demands ~ Gender * Background, data = dqi_respect)
+summary(mod)
+
+## Model respect with gender and background as predictors (without interaction) ##
+mod2 <- lm(Respect_for_demands ~ Gender + Background, data = dqi_respect)
+summary(mod2)
+
+## Model respect by incumbency ##
+mod3 <- lm(Respect_for_demands ~ Incumbent, data = dqi_respect)
+summary(mod3)
+
+## Model respect by gender ##
+mod4 <- lm(Respect_for_demands ~ Gender, data = dqi_respect)
+summary(mod4)
+
+## Model respect by background ##
+mod5 <- lm(Respect_for_demands ~ Background, data = dqi_respect)
+summary(mod5)
+
+## Model respect by political party affiliation ##
+mod6 <- lm(Respect_for_demands ~ Political_affiliation, data = dqi_respect)
+summary(mod6)
+
+## Model respect by gender, background, incumbency AND politicial affiliation ##
+mod7 <- lm(Respect_for_demands ~ Gender + Background + Incumbent + Political_affiliation, data = dqi_respect)
+summary(mod7)
+
+#### Data visualization ####
+library(ggplot2)
+#jpeg("whos_asking.jpeg", units="in", width=9, height=5, res=300) 
+
+dqi_viz <- dqi_by_speech_dem |>
+  drop_na(Interruption)
+
+ggplot(dqi_viz, aes(Interruption, Election_year/1000000)) +
+  geom_bar(stat='identity', fill="black") +
+  facet_wrap(~Political_affiliation) +
+  labs(
+    x = "Participation",
+    y = "Percentage of speeches",
+  ) +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  scale_y_continuous(labels=scales::percent) +
+  theme(strip.text.x = element_text(size = 14)) +
+  theme(axis.text.x =  element_text(size = 10)) +
+  theme(axis.text.y = element_text(size = 10)) + 
+  theme(axis.title.x = element_text(size = 14)) +
+  theme(axis.title.y = element_text(size = 14)) +
+  theme(legend.title = element_text(size = 12)) +
+  theme(legend.text = element_text(size = 10)) 
