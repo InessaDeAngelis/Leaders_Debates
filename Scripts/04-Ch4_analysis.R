6caa2530ed2ecc27ba3ef61635b201437c450ec7
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2025-02-22 20:48:34 -0500

    more models !

--- Scripts/04-Ch4_analysis.R
+++ Scripts/04-Ch4_analysis.R
@@ -14,14 +14,29 @@ dqi_by_speech_final <- read_csv("Outputs/Data/dqi_by_speech_final.csv")
 ## Add in demographic data ##
 dqi_by_speech_dem <- dqi_by_speech_final |>
   filter(Speaker == "Harper" | Speaker == "Dion" | Speaker == "May"| Speaker == "Layton" | Speaker == "Duceppe" |
-           Speaker == "Ignatieff" | Speaker == "Trudeau" | Speaker == "Singh" | Speaker == "Singh" | Speaker == "Paul" |
-           Speaker == "Mulcair" | Speaker == "Blanchette" | Speaker == "Scheer" | Speaker == "O'Toole") |>
+           Speaker == "Ignatieff" | Speaker == "Trudeau" | Speaker == "Singh" | Speaker == "Paul" |
+           Speaker == "Mulcair" | Speaker == "Blanchet" | Speaker == "Scheer" | Speaker == "O'Toole" | Speaker == "Bernier") |>
   mutate(Gender = if_else(Speaker %in% c("May", "Paul"), "Woman", "Man"), .after = Speaker) |>
   mutate(Background = if_else(Speaker %in% c("Singh", "Paul"), "Racialized", "White"), .after = Gender) |>
   mutate(Incumbent = if_else((Speaker == "Trudeau" & Election_year >= 2019) | 
                                (Speaker == "Harper" & Election_year <= 2015), 
                              "Incumbent", 
-                             "Not Incumbent"), .after = Background)
+                             "Not Incumbent"), .after = Background) |> 
+  mutate(Political_affiliation = case_when(
+    Speaker == "Harper" ~ "Conservative",
+    Speaker == "Dion" ~ "Liberal",
+    Speaker == "May" ~ "Green",
+    Speaker == "Layton" ~ "NDP",
+    Speaker == "Duceppe" ~ "Bloc Québécois",
+    Speaker == "Ignatieff" ~ "Liberal",
+    Speaker == "Trudeau" ~ "Liberal",
+    Speaker == "Singh" ~ "NDP",
+    Speaker == "Paul" ~ "Green",
+    Speaker == "Mulcair" ~ "NDP",
+    Speaker == "Blanchet" ~ "Bloc Québécois",
+    Speaker == "Scheer" ~ "Conservative",
+    Speaker == "O'Toole" ~ "Conservative",
+    Speaker == "Bernier" ~ "People's Party"), .after = Incumbent)
 
 #### Summary stats ####
 ## All speeches and interrupted speeches given by party leaders ##
@@ -68,16 +83,33 @@ speeches_gender_bg <- dqi_by_speech_dem |>
     percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
 speeches_gender_bg
 
+## All speeches and interrupted speeches by political party affiliation ##
+speeches_party <- dqi_by_speech_dem |>
+  group_by(Political_affiliation) |>
+  summarise(
+    interrupted_speeches = sum(Interruption == "Interruption", na.rm = TRUE),
+    total_speeches = n(),
+    percent_interrupted = round((sum(Interruption == "Interruption", na.rm = TRUE) / n()) * 100, 2))
+speeches_party
+
 #### Make models ####
 ## Analysis data ##
 dqi_in <-dqi_by_speech_dem |>
-  drop_na(Interruption)
+  drop_na(Interruption) |>
+  mutate(Political_affiliation = case_when(
+    Political_affiliation == "Conservative" ~ 1,
+    Political_affiliation == "Liberal" ~ 2,
+    Political_affiliation == "NDP" ~ 3,
+    Political_affiliation == "Green" ~ 4,
+    Political_affiliation == "Bloc Québécois" ~ 5,
+    Political_affiliation == "People's Party" ~ 6))
 
 ## Re-code variables ##
 dqi_in$Gender <- ifelse(dqi_in$Gender == "Man", 1, 0)
 dqi_in$Background <- ifelse(dqi_in$Background == "White", 1, 0)
 dqi_in$Incumbent <- ifelse(dqi_in$Incumbent == "Incumbent", 1, 0)
 dqi_in$Interruption <- ifelse(dqi_in$Interruption == "Interruption", 1, 0)
+dqi_in$Political_affiliation <- factor(dqi_in$Political_affiliation)
 
 ## Model interruptions with gender and background as predictors (with interaction) ##
 model <- lm(Interruption ~ Gender * Background, data = dqi_in)
@@ -98,3 +130,11 @@ summary(model4)
 ## Model interruptions by background ##
 model5 <- lm(Interruption ~ Background, data = dqi_in)
 summary(model5)
+
+## Model interruptions by political party affiliation ##
+model6 <- lm(Interruption ~ Political_affiliation, data = dqi_in)
+summary(model6)
+
+## Model interruptions by gender, background, incumbency AND politicial affiliation ##
+model7 <- lm(Interruption ~ Gender + Background + Incumbent + Political_affiliation, data = dqi_in)
+summary(model7)
